1d9f8f559c15ebf832650a65fe2590e0fc6aae89
shwanycom <shwanycom@gmail.com>
2019-02-22 15:36:00 +0900

    login창 연결 완료

---
+++ application/Main.java
@@ -0,0 +1,29 @@
+package application;
+
+import Controller.RootController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	public static void main(String[] args) {
+		launch(args);
+	}
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/login.fxml"));
+		Parent root = loader.load();
+		RootController rootController = loader.getController();
+		rootController.primaryStage = primaryStage;
+		
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("add.css").toString());
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Login");
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
+}
